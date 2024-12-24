089d4155106067b582bb63bd72ca5d16f0bb6810
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-24 22:57:28 +0530

    feat: wrap App component in StrictMode for improved development practices

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="logo">Pizza Buawna order now</h1>
-      <Order />
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo">Pizza Buawna order now</h1>
+        <Order />
+      </div>
+    </StrictMode>
   );
 };
 
